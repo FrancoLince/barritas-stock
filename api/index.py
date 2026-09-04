644939b00f6adadb8adece0a049af03271098719
FrancoLince <122633736+FrancoLince@users.noreply.github.com>
2026-09-04 20:12:24 -0300

    Remove database initialization code from index.py
    Removed database drop and create operations from index.py.

--- api/index.py
+++ api/index.py
@@ -1,5 +1,1 @@
-from app import app, db
-
-with app.app_context():
-    db.drop_all()    # Elimina todas las tablas existentes
-    db.create_all()  # Crea todas las tablas desde cero
+from app import app
